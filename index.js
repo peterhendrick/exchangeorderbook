a454fa7ede31dacabfc8f148d1cd0d28b0afc9d5
Peter Hendrick <Peterjhendrick@gmail.com>
2018-05-23 18:30:20 -0600

    removed unused code

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ app.get('/', function(req, res) {
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 app.use('/public', express.static(__dirname + '/public'));
 
-io.on('connection', function(socket) {
-    console.log('A new WebSocket connection has been established');
-});
-
-
 http.listen(8000, function() {
     console.log('Listening on *:8000');
 });
