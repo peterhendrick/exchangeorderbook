f00a90c37f6c66cf330b9004f1bf0597114200bf
Peter Hendrick <Peterjhendrick@gmail.com>
2018-05-24 19:45:10 -0600

    added DataTable jquery plugin

--- public/controller.js
+++ public/controller.js
@@ -1,8 +1,34 @@
 $(function () {
     let socket = io();
+    let bidTable;
+    let askTable;
+    let dtBid = $('#bidTable').DataTable({
+        show: false,
+        search: false
+    });
+    let dtAsk = $('#askTable').DataTable({
+        show: false,
+        search: false
+    });
 
     socket.on('combined books', function(orderBook) {
-        $('#stockprice').text(orderBook);
+        let asks = orderBook.asks;
+        let bids = orderBook.bids;
+        _createTable(bids, '#bidTable', dtBid);
+        _createTable(asks, '#askTable', dtAsk);
     });
+
+    function _createTable(orderBook, tableId, dataTable) {
+        dataTable.clear();
+        orderBook.forEach(order => {
+            let row = $('<tr>');
+            row.append($('<td>', {html: order.price}))
+                .append($('<td>', {html: order.volume}))
+                .append($('<td>', {html: order.exchange}))
+                .append($('<td>', {html: order.market}));
+            dataTable.rows.add(row);
+        });
+        dataTable.draw();
+    }
 
 });
