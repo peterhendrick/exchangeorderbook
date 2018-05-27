5a636f36d4a5d775ad5e3802e4d806fa1c884d81
Peter Hendrick <Peterjhendrick@gmail.com>
2018-05-26 19:54:27 -0600

    structured code and added text

--- public/controller.js
+++ public/controller.js
@@ -4,47 +4,25 @@ $(function () {
     let BTC_BCHOrderBook;
     let BTC_ETHTicker;
     let BTC_BCHTicker;
-    let bidDT = $('#bidTable').DataTable({
-        paging: false,
-        searching: false,
-        order: [[ 0, 'desc' ]],
-        lengthChange: false,
-        info: false,
-        autoWidth: false,
-        columnDefs: [{
-            "targets": '_all',
-            "createdCell": function (td) {
-                $(td).css('padding', '10px')
-            }
-        }]
-    });
-    let askDT = $('#askTable').DataTable({
-        paging: false,
-        searching: false,
-        order: [[ 0, 'asc' ]],
-        lengthChange: false,
-        info: false,
-        columnDefs: [{
-            "targets": '_all',
-            "createdCell": function (td) {
-                $(td).css('padding', '10px')
-            }
-        }]
-    });
-    $('#radio_1').on('click', function() {
-        $('#ticker').text(BTC_ETHTicker);
+
+    let bidDT = _initializeDataTable('#bidTable', 'desc');
+    let askDT = _initializeDataTable('#askTable', 'asc');
+
+    $('#BTC_ETH').on('click', function() {
+        $('#ticker').text(BTC_ETHTicker + ' BTC / ETH');
         _updateTables(BTC_ETHHOrderBook);
     }).click();
-    $('#radio_2').on('click', function() {
-        $('#ticker').text(BTC_BCHTicker);
+
+    $('#BTC_BCH').on('click', function() {
+        $('#ticker').text(BTC_BCHTicker + ' BTC / BCH');
         _updateTables(BTC_BCHOrderBook);
     });
 
     socket.on('combined BTC_ETH books', function(orderBook) {
         BTC_ETHHOrderBook = orderBook;
         if(orderBook.ticker) BTC_ETHTicker = orderBook.ticker;
-        if($('#radio_1').is(':checked')) {
-            $('#ticker').text(BTC_ETHTicker);
+        if($('#BTC_ETH').is(':checked')) {
+            $('#ticker').text(BTC_ETHTicker + ' BTC / ETH');
             _updateTables(BTC_ETHHOrderBook);
         }
     });
@@ -52,17 +30,50 @@ $(function () {
     socket.on('combined BTC_BCH books', function (orderBook) {
         BTC_BCHOrderBook = orderBook;
         if(orderBook.ticker) BTC_BCHTicker = orderBook.ticker;
-        if($('#radio_2').is(':checked')) {
-            $('#ticker').text(BTC_BCHTicker);
+        if($('#BTC_BCH').is(':checked')) {
+            $('#ticker').text(BTC_BCHTicker + ' BTC / BCH');
             _updateTables(BTC_BCHOrderBook);
         }
     });
+
+    function _initializeDataTable(id, sortOrder) {
+        return $(id).DataTable({
+            paging: false,
+            searching: false,
+            order: [[ 0, sortOrder ]],
+            lengthChange: false,
+            info: false,
+            autoWidth: false,
+            columnDefs: [{
+                "targets": '_all',
+                "createdCell": function (td) {
+                    $(td).css('padding', '10px')
+                }
+            }]
+        });
+    }
 
     function _updateTables(orderBook) {
         if(!orderBook) return;
         _setHighlights(orderBook);
         _createTable(orderBook.bids, bidDT, 'bids');
         _createTable(orderBook.asks, askDT, 'asks');
+    }
+
+    function _setHighlights(orderBook) {
+        let highestBid = _.head(orderBook.bids);
+        let lowestAsk = _.head(orderBook.asks);
+
+        orderBook.bids.forEach(bid => {
+            if(bid.price > lowestAsk.price) {
+                bid.highlight = true;
+            }
+        });
+        orderBook.asks.forEach(ask => {
+            if(ask.price < highestBid.price) {
+                ask.highlight = true;
+            }
+        });
     }
 
     function _createTable(orderBook, dataTable, type) {
@@ -85,19 +96,4 @@ $(function () {
         dataTable.draw();
     }
 
-    function _setHighlights(orderBook) {
-        let highestBid = _.head(orderBook.bids);
-        let lowestAsk = _.head(orderBook.asks);
-
-        orderBook.bids.forEach(bid => {
-            if(bid.price > lowestAsk.price) {
-                bid.highlight = true;
-            }
-        });
-        orderBook.asks.forEach(ask => {
-            if(ask.price < highestBid.price) {
-                ask.highlight = true;
-            }
-        });
-    }
 });
