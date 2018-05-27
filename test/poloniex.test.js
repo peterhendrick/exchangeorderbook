461679eef1eb08d9b41d17ac4ed28244c5c37db5
Peter Hendrick <Peterjhendrick@gmail.com>
2018-05-26 23:09:16 -0600

    tested poloniex module

--- test/poloniex.test.js
+++ test/poloniex.test.js
@@ -1,13 +1,21 @@
-const processResponse = require('../server/bittrex.js').formatOrderBook,
+const processResponse = require('../server/poloniex.js').processResponse,
+    processRESTResponse = require('../server/poloniex.js').formatRESTResponse,
     formattedResponseObject = require('./json/formattedDataObject.json'),
     expectedData = require('./expectedData.js')('Poloniex'),
-    mockResponse = require('./json/bittrexResponse.json'),
+    mockWebsocketResponse = require('./json/poloniexResponse.json').websocket,
+    mockRESTResponse = require('./json/poloniexResponse.json').REST,
     assert = require('assert');
 
 
 describe('Poloniex response handler', function () {
-    it('should process response properly', function (done) {
-        let formattedData = processResponse(mockResponse, formattedResponseObject);
+    it('should process websocket response properly', function (done) {
+        let formattedData = processResponse('BTC_BCH', mockWebsocketResponse, formattedResponseObject);
+        assert.deepEqual(formattedData, expectedData);
+        done();
+    });
+    it('should process REST response properly', function (done) {
+        let responseData = processRESTResponse(mockRESTResponse);
+        let formattedData = processResponse('BTC_BCH', responseData, formattedResponseObject);
         assert.deepEqual(formattedData, expectedData);
         done();
     });
