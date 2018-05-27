72a850889edad90d94a0ab23b6f1c83385a0ca6e
Peter Hendrick <Peterjhendrick@gmail.com>
2018-05-26 18:15:46 -0600

    updated ticker to immediatly update on market switch

--- public/controller.js
+++ public/controller.js
@@ -2,6 +2,8 @@ $(function () {
     let socket = io();
     let BTC_ETHHOrderBook;
     let BTC_BCHOrderBook;
+    let BTC_ETHTicker;
+    let BTC_BCHTicker;
     let bidDT = $('#bidTable').DataTable({
         paging: false,
         searching: false,
@@ -30,24 +32,28 @@ $(function () {
         }]
     });
     $('#radio_1').on('click', function() {
+        $('#ticker').text(BTC_ETHTicker);
         _updateTables(BTC_ETHHOrderBook);
     }).click();
     $('#radio_2').on('click', function() {
+        $('#ticker').text(BTC_BCHTicker);
         _updateTables(BTC_BCHOrderBook);
     });
 
     socket.on('combined BTC_ETH books', function(orderBook) {
         BTC_ETHHOrderBook = orderBook;
+        if(orderBook.ticker) BTC_ETHTicker = orderBook.ticker;
         if($('#radio_1').is(':checked')) {
-            if(orderBook.ticker) $('#ticker').text(orderBook.ticker);
+            $('#ticker').text(BTC_ETHTicker);
             _updateTables(BTC_ETHHOrderBook);
         }
     });
 
     socket.on('combined BTC_BCH books', function (orderBook) {
         BTC_BCHOrderBook = orderBook;
+        if(orderBook.ticker) BTC_BCHTicker = orderBook.ticker;
         if($('#radio_2').is(':checked')) {
-            if(orderBook.ticker) $('#ticker').text(orderBook.ticker);
+            $('#ticker').text(BTC_BCHTicker);
             _updateTables(BTC_BCHOrderBook);
         }
     });
